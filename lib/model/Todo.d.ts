990445b88d88d3f9caac1bfb92f7f78e027355bc
jrebecchi <johann.corcuff@gmail.com>
2020-04-24 02:09:42 +0200

    Update .gitignore

--- lib/model/Todo.d.ts
+++
@@ -1,8 +0,0 @@
-import mongoose, { Document } from 'mongoose';
-export interface Todo extends Document {
-    text: string;
-    userId: string;
-    isCompleted: boolean;
-}
-declare const _default: mongoose.Model<Todo, {}>;
-export default _default;
